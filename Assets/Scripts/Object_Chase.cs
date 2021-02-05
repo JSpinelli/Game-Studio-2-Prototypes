fc1ed394049681745bfe36fac71d99b17d899e75
Walid <ar6749@nyu.edu>
2021-02-05 11:53:08 -0500

    Chasing Script
    Added a chase script and temporary movement on baby

---
+++ Assets/Scripts/Object_Chase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Object_Chase : MonoBehaviour
+{
+
+    public Transform targ;
+    public float offset;
+    public float speed;
+
+
+    void Update()
+    {
+        transform. position = Vector3. Lerp(transform. position, targ. position, speed * Time. deltaTime);
+        transform. rotation = Quaternion. Slerp(transform.rotation, targ. rotation, speed * Time. deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, targ.transform.position, offset );
+    }
+}
