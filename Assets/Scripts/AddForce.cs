46c8336421636ceffb7f9ea3087611173e886386
Walid <ar6749@nyu.edu>
2021-02-08 15:16:15 -0500

    Added a pick up/throw mechanic
    It's a little janky, needs a bit of tuning, but it works

--- Assets/Scripts/AddForce.cs
+++ Assets/Scripts/AddForce.cs
@@ -20,11 +20,11 @@ public class AddForce : MonoBehaviour
     void FixedUpdate()
     {
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+       // float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+        //GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
     }
 }
