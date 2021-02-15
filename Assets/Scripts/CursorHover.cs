03091f36f362665e67de4e5addbb1603ee2c416a
Walid <ar6749@nyu.edu>
2021-02-15 04:13:33 -0500

    Added Raycast to interact with Colliders + A WiP hover system
    Just need to add the door moving stuff and it should work

---
+++ Assets/Scripts/CursorHover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorHover : MonoBehaviour
+{
+   [SerializeField] private Image customImage;
+
+   void OnMouseOver(Collider other)
+   {
+      if (other.CompareTag("Interactable"))
+      {
+         customImage.enabled = true;
+      }
+      else
+      {
+         customImage.enabled = false;
+      }
+   } 
+}
--- Assets/Scripts/WalkUpAndInteract.cs
+++ Assets/Scripts/WalkUpAndInteract.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interact : MonoBehaviour
+public class WalkUpAndInteract : MonoBehaviour
 {
    public GameObject text;
  
