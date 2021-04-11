59f45915caaa1691f7ec6cfc2fc76456c01060a0
Julian <julianspi95@gmail.com>
2021-04-11 12:01:11 -0300

    Portrait Fix and Filler sequence tweak

--- Assets/Scripts/KitchenSequence.cs
+++ Assets/Scripts/KitchenSequence.cs
@@ -182,7 +182,7 @@ public class KitchenSequence : MonoBehaviour
         else
         {
             _fillerTimerCounter = 0;
-            PlayFiller();
+            //PlayFiller();
         }
     }
 }
