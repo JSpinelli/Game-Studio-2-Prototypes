5c67d74e87a1766e8d674dfdfd1ecee3078da99b
Julian <julianspi95@gmail.com>
2021-05-03 16:37:35 -0300

    Head Tracking script test

---
+++ Assets/HeadTracking.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadTracking : MonoBehaviour
+{
+
+    public Transform ToTrack;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.LookAt(ToTrack);
+    }
+}
